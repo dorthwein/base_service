5c471e6111921faa58063e17d212e2fe2a47b335
Daniel Orthwein <dorthwein@gmail.com>
2015-08-21 13:42:00 -0500

    update

--- lib/base_service.rb
+++ lib/base_service.rb
@@ -59,7 +59,7 @@ class BaseService
 		clean_params = {}
 #			clean_params[:id] = params[:id] if !params[:id].nil?
 		params.each do |key, value|
-			clean_params[key.to_sym] = params[key] if object.respond_to?(key.to_s + '=')
+			clean_params[key.to_sym] = params[key] if object.respond_to?(key.to_s + '=') && key != 'id' && key != :id
 		end
 		return clean_params
 	end
